6d29ccdcf091c954b405b974dc5551ea37da3012
Diego Mixfarma <diegohti.mixfarma@gmail.com>
2018-08-30 17:32:39 -0300

    Ajustes no ".env", deixando o Laravel em pt-BR. Ajustes no "ProvideService" para o funcionamento correto do MariaDB

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,24 +3,27 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+    * Bootstrap any application services.
+    *
+    * @return void
+    */
+
+
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
-     * Register any application services.
-     *
-     * @return void
-     */
+    * Register any application services.
+    *
+    * @return void
+    */
     public function register()
     {
         //
